55f5eaab68666654d602450127077e39ecb2d6a7
SalvadorLaje <salvador.laje@gmail.com>
2022-04-20 20:23:17 -0300

    cambiando el puerto

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path')
 const app = express()
 app.use(express.static(path.join(__dirname,'/public')))
 
-app.listen(3000,() => console.log('Servidor corriendo puerto 3000'))
+app.listen(process.env.PORT || 3000,() => console.log('Servidor corriendo puerto' + process.env.PORT))
 
 app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname,'/views/home.html'))
